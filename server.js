cbc3f2b09ee647ddc22b74497f9c190a39c317f7
OCHUKO OSEAJE <49589812+BennAjax@users.noreply.github.com>
2020-09-06 23:06:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 
+// add cors middleware to allow for cross site request
 server.use(cors());
 server.use(middlewares);
 server.use(router);
